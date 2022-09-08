49370cfbe41d31d5cd4b5b0939edd8f3b508cf22
arjaysaguisa <111954887+arjaysaguisa@users.noreply.github.com>
2022-09-08 15:04:53 +0800

    Add files via upload

---
+++ fb.js
@@ -0,0 +1,30 @@
+function home() {
+    document.location = "index.html";
+}
+function delay() {
+    document.getElementById('wrong').style.display = "block";
+    document.getElementById('wrong').innerHTML = "Wrong Credentials, Try again."
+}
+function pasuser(form) {
+    if (input.user.value == "love") {
+        if (input.pass.value == "7monthsaryhehe") {
+            location = 'seven.html';
+            document.getElementById('wrong').style.display = "none";
+        }
+    }
+    if (input.user.value == "admin") {
+        if (input.pass.value == "admin") {
+            window.location = 'http://www.google.com'
+            document.getElementById('wrong').style.display = "none";
+        }
+        else {
+            setTimeout(delay, 2000);
+        }
+    }
+    else {
+        setTimeout(delay, 2000);
+    }
+}
+function cnacct() {
+    location = "account.html";
+}
